1df02c36d31f1fa0967d1ebe7211bc048aa6172b
Anders Bjerner <abjerner@skybrud.dk>
2019-02-04 15:38:15 +0100

    SpaRequest stuff

--- src/Skybrud.Umbraco.Spa/Models/SpaRequest.cs
+++ src/Skybrud.Umbraco.Spa/Models/SpaRequest.cs
@@ -1,14 +1,30 @@
 ﻿using System;
+using System.Globalization;
+using System.Web;
 using Newtonsoft.Json;
 using Skybrud.Essentials.Enums;
 using Skybrud.Umbraco.Spa.Extensions;
 using Umbraco.Core.Models;
+using Umbraco.Web;
 
 namespace Skybrud.Umbraco.Spa.Models {
 
     public class SpaRequest {
 
         #region Properties
+
+        public static SpaRequest Current {
+            get {
+                if (System.Web.HttpContext.Current == null) return null;
+                return System.Web.HttpContext.Current.Items["SpaApiRequest"] as SpaRequest;
+            }
+            set => System.Web.HttpContext.Current.Items["SpaApiRequest"] = value;
+        }
+
+        /// <summary>
+        /// Gets a reference to the HTTP context of the request.
+        /// </summary>
+        public HttpContextBase HttpContext { get; }
 
         /// <summary>
         /// Gets or sets the ID of the site.
@@ -59,6 +75,24 @@ namespace Skybrud.Umbraco.Spa.Models {
 
         public object ContentModel { get; set; }
 
+        /// <summary>
+        /// Gets the virtual parent if present; otherwise <c>null</c>.
+        /// 
+        /// A virtual parent is typically used when a content item should appear under another node than it's own parent.
+        /// </summary>
+        [JsonIgnore]
+        public IPublishedContent VirtualParent { get; set; }
+
+        /// <summary>
+        /// Gets whether the request has a virtual parent.
+        /// </summary>
+        [JsonIgnore]
+        public bool HasVirtualParent => VirtualParent != null;
+
+        public bool IsDanish => CultureInfo.CurrentCulture.TwoLetterISOLanguageName == "da";
+
+        public bool IsEnglish => CultureInfo.CurrentCulture.TwoLetterISOLanguageName == "en";
+
         #endregion
 
         #region Constructors
@@ -67,7 +101,9 @@ namespace Skybrud.Umbraco.Spa.Models {
         /// Initializes a new a instance with default options.
         /// </summary>
         [JsonConstructor]
-        public SpaRequest() { }
+        public SpaRequest() {
+            HttpContext = new HttpContextWrapper(System.Web.HttpContext.Current);
+        }
 
         /// <summary>
         /// Intializes a new instance for the site with the specified <paramref name="siteId"/>.
@@ -78,6 +114,7 @@ namespace Skybrud.Umbraco.Spa.Models {
             Url = "";
             IsPreview = false;
             Parts = GetParts();
+            HttpContext = new HttpContextWrapper(System.Web.HttpContext.Current);
         }
 
         public SpaRequest(int siteId, string url) {
@@ -85,6 +122,7 @@ namespace Skybrud.Umbraco.Spa.Models {
             Url = url;
             IsPreview = url.IsPreviewUrl();
             Parts = GetParts();
+            HttpContext = new HttpContextWrapper(System.Web.HttpContext.Current);
         }
 
         public SpaRequest(int siteId, string url, string parts) {
@@ -92,6 +130,7 @@ namespace Skybrud.Umbraco.Spa.Models {
             Url = String.IsNullOrWhiteSpace(url) ? "/" : url;
             IsPreview = Url.IsPreviewUrl();
             Parts = GetParts(parts);
+            HttpContext = new HttpContextWrapper(System.Web.HttpContext.Current);
         }
 
         public SpaRequest(int siteId, IPublishedContent site, string url, string parts) {
@@ -100,6 +139,16 @@ namespace Skybrud.Umbraco.Spa.Models {
             Url = String.IsNullOrWhiteSpace(url) ? "/" : url;
             IsPreview = Url.IsPreviewUrl();
             Parts = GetParts(parts);
+            HttpContext = new HttpContextWrapper(System.Web.HttpContext.Current);
+        }
+
+        public SpaRequest(UmbracoContext umbraco, int siteId, IPublishedContent site, string url, string parts) {
+            SiteId = siteId;
+            Site = site;
+            Url = String.IsNullOrWhiteSpace(url) ? "/" : url;
+            IsPreview = Url.IsPreviewUrl();
+            Parts = GetParts(parts);
+            HttpContext = umbraco.HttpContext;
         }
 
         #endregion

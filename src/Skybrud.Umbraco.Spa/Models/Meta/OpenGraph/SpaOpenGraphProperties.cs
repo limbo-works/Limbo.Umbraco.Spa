624c3cc457c77581d73f697cf3d3ed49f813b75a
Anders Bjerner <abjerner@skybrud.dk>
2019-10-01 11:43:45 +0200

    Cleanup

--- src/Skybrud.Umbraco.Spa/Models/Meta/OpenGraph/SpaOpenGraphProperties.cs
+++ src/Skybrud.Umbraco.Spa/Models/Meta/OpenGraph/SpaOpenGraphProperties.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
-using Umbraco.Core.Models;
 using Umbraco.Core.Models.PublishedContent;
 using Umbraco.Web;
 

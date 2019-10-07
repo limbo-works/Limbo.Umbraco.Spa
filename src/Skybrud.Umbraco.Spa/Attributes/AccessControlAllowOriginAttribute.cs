cc2db0afe7bf943594bbae068bca635baf508658
Anders Bjerner <abjerner@skybrud.dk>
2019-10-07 10:59:17 +0200

    SpaMicroCacheAttribute should have it's own file

--- src/Skybrud.Umbraco.Spa/Attributes/AccessControlAllowOriginAttribute.cs
+++ src/Skybrud.Umbraco.Spa/Attributes/AccessControlAllowOriginAttribute.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Net.Http;
-using System.Web;
-using System.Web.Http.Controllers;
+﻿using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
-using Skybrud.Umbraco.Spa.Models;
-using Umbraco.Core.Cache;
-using Umbraco.Core.Composing;
 
 namespace Skybrud.Umbraco.Spa.Attributes {
 
@@ -13,41 +7,6 @@ namespace Skybrud.Umbraco.Spa.Attributes {
 
         public override void OnActionExecuting(HttpActionContext actionContext) {
             actionContext.Request.Headers.Add("Access-Control-Allow-Origin", "*");
-        }
-
-    }
-
-    public class SpaMicroCacheAttribute : ActionFilterAttribute {
-        
-        public SpaRequestOptions Options { get; private set; }
-
-        public string CacheKey { get; private set; }
-
-        public override void OnActionExecuting(HttpActionContext context) {
-
-            Options = new SpaRequestOptions(new HttpContextWrapper(HttpContext.Current));
-
-            if (Options.EnableCaching == false) return;
-
-            CacheKey = Options.CacheKey;
-
-            IAppPolicyCache cache = Current.AppCaches.RuntimeCache;
-
-            HttpResponseMessage cachedModel = cache.Get(CacheKey) as HttpResponseMessage;
-            if (cachedModel == null) return;
-
-            context.Response = cachedModel;
-
-        }
-
-        public override void OnActionExecuted(HttpActionExecutedContext context) {
-
-            if (Options.EnableCaching == false) return;
-
-            IAppPolicyCache cache = Current.AppCaches.RuntimeCache;
-
-            cache.InsertCacheItem(Options.CacheKey, () => context.Response, TimeSpan.FromSeconds(60));
-
         }
 
     }
---
+++ src/Skybrud.Umbraco.Spa/Attributes/SpaMicroCacheAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using Skybrud.Umbraco.Spa.Models;
+using Umbraco.Core.Cache;
+using Umbraco.Core.Composing;
+
+namespace Skybrud.Umbraco.Spa.Attributes {
+
+    public class SpaMicroCacheAttribute : ActionFilterAttribute {
+        
+        public SpaRequestOptions Options { get; private set; }
+
+        public string CacheKey { get; private set; }
+
+        public override void OnActionExecuting(HttpActionContext context) {
+
+            Options = new SpaRequestOptions(new HttpContextWrapper(HttpContext.Current));
+
+            if (Options.EnableCaching == false) return;
+
+            CacheKey = Options.CacheKey;
+
+            IAppPolicyCache cache = Current.AppCaches.RuntimeCache;
+
+            HttpResponseMessage cachedModel = cache.Get(CacheKey) as HttpResponseMessage;
+            if (cachedModel == null) return;
+
+            context.Response = cachedModel;
+
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext context) {
+
+            if (Options.EnableCaching == false) return;
+
+            IAppPolicyCache cache = Current.AppCaches.RuntimeCache;
+
+            cache.InsertCacheItem(Options.CacheKey, () => context.Response, TimeSpan.FromSeconds(60));
+
+        }
+
+    }
+
+}

2c00a524f17c32b0ad349c63ef9ff8029100ef0a
Anders Bjerner <abjerner@skybrud.dk>
2019-10-09 10:15:47 +0200

    XML documentation

--- src/Skybrud.Umbraco.Spa/Models/SpaLinkItem.cs
+++ src/Skybrud.Umbraco.Spa/Models/SpaLinkItem.cs
@@ -1,29 +1,55 @@
-﻿using System;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Umbraco.Core.Models.PublishedContent;
 
 namespace Skybrud.Umbraco.Spa.Models {
 
+    /// <summary>
+    /// Class representing a simple link.
+    /// </summary>
     public class SpaLinkItem {
 
+        /// <summary>
+        /// Gets the URL of the link.
+        /// </summary>
         [JsonProperty("url")]
         public string Url { get; }
 
+        /// <summary>
+        /// Gets the <c>target</c> attribute of the link.
+        /// </summary>
         [JsonProperty("target")]
         public string Target { get; }
         
+        /// <summary>
+        /// Initializes a new link based on the specified <paramref name="url"/>.
+        /// </summary>
+        /// <param name="url">The URL of the link.</param>
         protected SpaLinkItem(string url) {
             Url = url;
         }
 
+        /// <summary>
+        /// Initializes a new link based on the specified <paramref name="content"/>.
+        /// </summary>
+        /// <param name="content">The instance of <see cref="IPublishedContent"/> the link should be based on.</param>
         protected SpaLinkItem(IPublishedContent content) {
             Url = content.Url;
         }
 
+        /// <summary>
+        /// Initializes a new link based on the specified <paramref name="content"/>.
+        /// </summary>
+        /// <param name="content">The instance of <see cref="IPublishedContent"/> the link should be based on.</param>
+        /// <returns>An instance of <see cref="SpaLinkItem"/>.</returns>
         public static SpaLinkItem GetFromContent(IPublishedContent content) {
             return content == null ? null : new SpaLinkItem(content);
         }
 
+        /// <summary>
+        /// Initializes a new link based on the specified <paramref name="url"/>.
+        /// </summary>
+        /// <param name="url">The URL of the link.</param>
+        /// <returns>An instance of <see cref="SpaLinkItem"/>.</returns>
         public static SpaLinkItem GetFromUrl(string url) {
             return string.IsNullOrWhiteSpace(url) ? null : new SpaLinkItem(url);
         }

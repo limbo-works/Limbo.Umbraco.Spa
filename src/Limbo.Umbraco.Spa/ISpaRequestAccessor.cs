8dc08215b6af9c34b18ffd94187e53e99fd0f57e
Anders Bjerner <abjerner@skybrud.dk>
2022-05-11 14:13:07 +0200

    Introduced new ISpaRequestAccessor
    This introduces a way to get the current "SpaRequest" using dependency injection the proper way.

---
+++ src/Limbo.Umbraco.Spa/ISpaRequestAccessor.cs
@@ -0,0 +1,17 @@
+﻿using Limbo.Umbraco.Spa.Models;
+
+namespace Limbo.Umbraco.Spa {
+    
+    /// <summary>
+    /// Interface describing a way to access the current <see cref="SpaRequest"/>.
+    /// </summary>
+    public interface ISpaRequestAccessor {
+
+        /// <summary>
+        /// Gets a reference to the current SPA request, if any.
+        /// </summary>
+        SpaRequest Current { get; set; }
+
+    }
+
+}

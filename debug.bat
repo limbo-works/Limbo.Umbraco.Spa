3f2f77e6baef66906c45f5d93e529b896419ae56
Anders Bjerner <abjerner@skybrud.dk>
2024-05-08 14:35:04 +0200

    Build stuff

--- debug.bat
+++ debug.bat
@@ -1,3 +1,3 @@
 @echo off
 
-dotnet build src/Limbo.Umbraco.Spa --configuration Debug /t:rebuild /t:pack -p:BuildTools=1 -p:PackageOutputPath=c:/nuget
+dotnet build src/Limbo.Umbraco.Spa --configuration Debug /t:rebuild /t:pack -p:PackageOutputPath=c:\nuget\Umbraco13
